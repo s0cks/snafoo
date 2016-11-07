6d50ebec1911c91d436b5db2439ad7fc06eb7c81
s0cks <tasman.vose@gmail.com>
2016-11-07 17:11:43 +0000

    Update model/rest/Suggestion

--- src/main/java/io/github/s0cks/snafoo/model/rest/Suggestion.java
+++ src/main/java/io/github/s0cks/snafoo/model/rest/Suggestion.java
@@ -3,10 +3,16 @@ package io.github.s0cks.snafoo.model.rest;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+/**
+ * This is another Java JSON object representation class.
+ * This represents the JSON object that gets POSTed to the Snack API endpoint.
+ * For sanity sake this object is first sent to the app from the client and then to the final API endpoint.
+ */
 public final class Suggestion{
   public final String name;
   public final String location;
 
+  // Another JSON deserialisation constructor.
   @JsonCreator
   private Suggestion(@JsonProperty("Name") String name,
                      @JsonProperty("Location") String location){
