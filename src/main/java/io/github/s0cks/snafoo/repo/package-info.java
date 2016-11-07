12fb65ca272d358778702df31bae50916ce8c2e9
s0cks <tasman.vose@gmail.com>
2016-11-07 16:44:07 +0000

    Update repo/package-info.java

--- src/main/java/io/github/s0cks/snafoo/repo/package-info.java
+++ src/main/java/io/github/s0cks/snafoo/repo/package-info.java
@@ -1,5 +1,13 @@
 /**
+ * Since this package contains simple interfaces that are extremely obvious I'll just breifly elaborate on them.
  * 
+ * Each interface in this package are just declarations for JPA repositories. The name schema represents the repositories element type i.e:
+ * UserRepository - JPA repository for {@link io.github.s0cks.snafoo.model.domain.User}
+ * 
+ * Any more detailed information can be found just by reading the declarations - the functions contained in there should be verbose enough to see what they do
+ * 
+ * Side Note:
+ *   Why does Spring? JPA do this weird (what I am assuming is) class loader magic with either generation via ASM or mixing in functonalities with ASM either way its weird.
  *
  */
 package io.github.s0cks.snafoo.repo;
