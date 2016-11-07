4146bbcdd156fd218de58b815805b935b9465c1d
s0cks <tasman.vose@gmail.com>
2016-11-07 17:06:50 +0000

    Update Snack.java

--- src/main/java/io/github/s0cks/snafoo/model/rest/Snack.java
+++ src/main/java/io/github/s0cks/snafoo/model/rest/Snack.java
@@ -5,6 +5,9 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.Comparator;
 
+/**
+ * Basic Java representation of the Snack JSON Object.
+ */
 public class Snack {
   public final int id;
   public final int purchaseCount;
@@ -13,6 +16,9 @@ public class Snack {
   public final String lastPurchaseDate;
   public final boolean optional;
 
+  // Doesn't necessarily need to be private for this Copy constructor (I'm already missing C++'s = overloading for copy constructors :<)
+  // However for sake of sanity and promotion of safe coding I made it private and the use of it is contained in a static inner class
+  // Also bring friend classes to Java plz
   private Snack(Snack s){
     this.id = s.id;
     this.purchaseCount = s.purchaseCount;
@@ -22,6 +28,8 @@ public class Snack {
     this.optional = s.optional;
   }
 
+  // This is also private for the same reason however the usage is reflective and access has been changed to public so no need to worry
+  // This just details how to deserialise the object from the JSON object via constructor call since reflection is slow and evil
   @JsonCreator
   private Snack(@JsonProperty("Id") int id,
                 @JsonProperty("Name") String name,
@@ -37,10 +45,13 @@ public class Snack {
     this.lastPurchaseDate = date;
   }
 
+  // This one-off inner class is a hackish way of safely mutating the immutable snack (weird right?)
+  // Its an extension of Snack but takes a Snack instance in its constructor to clone its values into a new instance.
+  // This just adds a couple fields for the internal rest service of this app.
   public static final class VotedSnack
   extends Snack{
-    public final boolean voted;
-    public final int votes;
+    public final boolean voted; // Wether or not that this user has voted on this object
+    public final int votes; // The number of votes that this object has
 
     public VotedSnack(Snack owner, int votes, boolean voted){
       super(owner);
@@ -49,6 +60,11 @@ public class Snack {
     }
   }
 
+  // These are just some utility comparators that I didn't want to stuff into a whole new class for conservation purposes
+  // Excuse the excessive use of Java8/Functional paradigms but I'm quite fond of them. I would have used Scala for this if I chose to not use Spring stuff
+  // This comparator sorts Snack objects by name
   public static final Comparator<Snack> SORT_BY_NAME = (t1, t2) -> t1.name.compareToIgnoreCase(t2.name);
+  // This comparator sorts VotedSnack objects by votes
   public static final Comparator<VotedSnack> SORT_BY_VOTES = (t1, t2) -> Integer.compare(t1.votes, t2.votes);
-}
+  // Their uses are in the controller class
+}  
